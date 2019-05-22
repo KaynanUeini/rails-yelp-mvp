04d1183277c5bc66eb0be13676a04a016d12ed09
Kaynan Silva <kaynan_alves@hotmail.com>
2019-05-22 16:26:41 -0300

    Finished exercise without the front-end

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,26 @@
 class ReviewsController < ApplicationController
-  def index
-  end
-
+  before_action :set_restaurant
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(reviews_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def reviews_params
+    params.require(:review).permit(:content, :rating)
   end
 end
